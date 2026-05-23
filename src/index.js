e4744b5577492dc8fda49f5080cbe5346610f127
utkarsh <utkarsh1228@gmail.com>
2026-05-23 13:02:13 +0530

    seperate db connect and creatge utils

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION FAILED!!", error);
+  });
 
-/*
+/*  WE MODULARIZE THIS BY SHIFTING IT TO db/index.js
+
 import express from "express";
 const app = express()(async () => {
   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//WRAPPER TO HANDLE ASYNC AWAITS OF DB CONNECT
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {};
+// const asyncHandler = (func) => {() => {}}
+//******same as below*******
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {   
+  try {
+    await fn(req,res,next)
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};*/
