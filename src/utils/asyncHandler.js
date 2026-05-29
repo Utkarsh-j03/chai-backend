ba7169922b6ac9dc19fed5721638f11dee431090
utkarsh <utkarsh1228@gmail.com>
2026-05-29 13:43:08 +0530

    user routes and controller (all setup complete)

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //WRAPPER TO HANDLE ASYNC AWAITS OF DB CONNECT
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) => next(err));
   };
 };
